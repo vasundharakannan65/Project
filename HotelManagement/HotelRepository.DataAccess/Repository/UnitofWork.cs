1ddecd0f27cc5eb40b79a1c211798249d084bbfa
vasundharakannan65 <vasundharakannan65@gmail.com>
2021-09-01 03:01:41 +0530

    Completed menu,pending repo pattern

---
+++ HotelManagement/HotelRepository.DataAccess/Repository/UnitofWork.cs
@@ -0,0 +1,33 @@
+﻿using HotelRepository.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRepository.DataAccess.Repository
+{
+    public class UnitofWork : IUnitofWork
+    {
+        private readonly ApplicationDbContext _db;
+
+
+        public UnitofWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+
+        }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+    }
+}
